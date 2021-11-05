4a9619cfc3c3852d022e445303e03cbb705cd240
rigelsolutions@outlook.com <rigelsolutions@outlook.com>
2021-11-05 19:15:20 +0300

    added africastalking amd finished OTP

--- routes/auth.js
+++ routes/auth.js
@@ -13,6 +13,7 @@ const generalrateLimiterMiddleware = require("../middlewares/rateLimiters/generi
 const jwt = require('jsonwebtoken');
 const { RateLimiterRedis } = require('rate-limiter-flexible');
 const sendOTP = require("../middlewares/aws_sns");
+const twilioSMS = require("../middlewares/twilioSMS");
 
 const router = express.Router();
 
@@ -22,16 +23,16 @@ const schema = Joi.object({
     email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
     first_name: Joi.string().required().max(20).min(3).regex(/^[A-Za-z]+$/).error(new Error("Invalid First Name")),
     last_name: Joi.string().required().max(20).min(3).regex(/^[A-Za-z]+$/).error(new Error("Invalid Last Name")),
-    location:Joi.string().required(),
+    location: Joi.string().required(),
     contact: Joi.string().trim().regex(/^[0-9]{12,13}$/).error(new Error('Invalid Phone number')),
     avatar: Joi.string()
 });
 const loginschema = Joi.object({
-    contact: Joi.string().required(),
-    
+    contact: Joi.string().required().min(13).max(14).error(new Error("Invalid Phone Number")),
+
 });
 
-const nanoid = customAlphabet('1234567890abcdef', 6)
+const nanoid = customAlphabet('1234567890', 6)
 const storage = multer.diskStorage({
 
     filename: function (req, file, cb) {
@@ -66,7 +67,7 @@ router.post('/users/create', uploads.single("avatar"), async (req, res) => {
 
         const contactexist = await Users.findOne({ contact: req.body.contact });
         if (contactexist) {
-           return res.status(400).json({ message: `Contact ${req.body.contact} already exist` })
+            return res.status(400).json({ message: `Contact ${req.body.contact} already exist` })
         }
 
         //uploading image to cloudinary
@@ -74,8 +75,8 @@ router.post('/users/create', uploads.single("avatar"), async (req, res) => {
         const file = req.file
 
         let result
-        if(file){
-           result= await cloudinary.uploader.upload(file.path)
+        if (file) {
+            result = await cloudinary.uploader.upload(file.path)
         };
         //create new user object after validation and hashing
 
@@ -89,17 +90,19 @@ router.post('/users/create', uploads.single("avatar"), async (req, res) => {
         });
         //try to save user 
 
-        await user.save()
-        sendOTP(req.body.contact).then(result=>{
+        await user.save().then(result => {
+            const otp_code = nanoid();
+            twilioSMS(`Welcome! your mobile verification code is: ${otp_code}. Mobile Number is: ${mobileno}`, req.body.contact)
             return res.status(200).json({ message: "Account registered successfully, Please proceed to Login" });
-        })
-        
+        });
+
+
 
 
 
     } catch (error) {
         logger.error(`${error.status || 500} - ${req.body.contact} - ${req.body.email} - ${error.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-       return res.status(400).json({ message: error.message })
+        return res.status(400).json({ message: error.message })
     }
 
 
@@ -113,97 +116,106 @@ const opts = {
     points: 5, // 5 points
     duration: 15 * 60, // Per 15 minutes
     blockDuration: 15 * 60, // block for 15 minutes if more than points consumed 
-  };
-  
+};
+
 const rateLimiter = new RateLimiterRedis(opts);
 
 router.post('/user/login', async (req, res) => {
-
-    const { bodyError, value } = await loginschema.validateAsync(req.body);
     try {
-        if (bodyError) {
-            res.status(400).json({ message: bodyError });
-        } else {
-            //check if contact  exist in database
-            const user = await Users.findOne({ contact: req.body.contact });
+         const {bodyError,value }= await loginschema.validateAsync(req.body)
 
-            if (!user) {
-                res.status(400).json({ message: "Account doesn't not exist" })
-            } else {
-                //send otp and save in the redis db
-                sendOTP(user.contact).then(result=>{
-                    res.status(200).json({ message: "Account verified, submit the OTP code to login" })
-                })
-               
+            
+                //check if contact  exist in database
+                const user =await Users.findOne({ contact: req.body.contact });
 
-            }
-        }
+                if (!user) {
+                    res.status(400).json({ message: "Account doesn't not exist" })
+                } else {
+                    //send otp and save in the redis db
+                    const otp_code = nanoid();
+                    const redisField=`${user.contact}OTP`
+                   await redisClient.set(redisField.toString(),otp_code.toString(),"EX",180,(err,result)=>{
+                      
+                            if(err){
+                                return res.status(400).json({ message: err })
+                            }
+                            twilioSMS(`Hello ${user.first_name} your verification code is: ${otp_code}. Mobile Number is: ${user.contact}`, user.contact).then(reply=>{
+                            return res.status(200).json(`Hello ${user.first_name} your verification code is: ${otp_code}. Mobile Number is: ${user.contact}`)
+                        }).catch(e=>{return res.status(400).json({message:e})})
+                        
+                    })
+                    
+                }
+
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
 })
 //verify otpcode and assign jwt
-router.post("/user/otpverify", async (req,res)=>{
+router.post("/user/otpverify", async (req, res) => {
     const otpCode = req.body.otp_code
     const contact = req.body.contact
     try {
-        if(otpCode){
+        if (otpCode) {
             const user = await Users.findOne({ contact: contact });
-    
-                if (!user) {
-                    res.status(400).json({ message: "Account doesn't not exist" })
-                } else {
-            //compare code in redis with the ones sent
-             await redisClient.get(contact.toString(),(err,redisData)=>{
-                if(err){ return logger.error(err)};
-                           console.log(redisData)
-                if(redisData ===null || JSON.parse(redisData).redisCode != otpCode ) {
-                 // Consume 1 point for each failed login attempt
-                 rateLimiter.consume(req.socket.remoteAddress)
-                 .then((data) => {
-                     // Message to user
-                    return res.status(400).json({message:`Invalid Code, you have ${data.remainingPoints} attempts left, Please Login Again`});
-                 })
-                 .catch((rejRes) => {
-                     // Blocked
-                     const secBeforeNext = Math.ceil(rejRes.msBeforeNext / 60000) || 1;
-                     logger.error(`LoggingIn alert: Contact: ${req.body.contact} on IP: ${req.socket.remoteAddress} is Chocking Me !!`)
-                    return res.status(429).send(`Too Many Requests, Try to Login After ${String(secBeforeNext)} Minutes`);
-                 });
-            
+
+            if (!user) {
+                res.status(400).json({ message: "Account doesn't not exist" })
+            } else {
+                const string=`${contact}OTP`
                 
-                }else{
-    
-                    //create and assign a token once code is verified
-    
-                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.TOKEN_SECRET, { expiresIn: 120 })
-    
-    
-                    const refreshToken = jwt.sign({ _id: user._id,role: user.role }, process.env.REFRESH_TOKEN_SECRET,
-                        { expiresIn: '1d' });
-    
-                     redisClient.set(user._id.toString(), JSON.stringify({ refreshToken: refreshToken }));
-                     
-                    const userInfo = {
-                        _id: user._id,
-                        first_name: user.first_name,
-                        last_name: user.last_name,
-                        contact: user.contact,
-                        avatar: user.avatar
+                //compare code in redis with the ones sent
+                await redisClient.get(string.toString(), (err, redisData) => {
+                    if (err) { return logger.error(err) };
+                    
+                    if (redisData === null || redisData != otpCode) {
+                        // Consume 1 point for each failed login attempt
+                        rateLimiter.consume(req.socket.remoteAddress)
+                            .then((data) => {
+                                // Message to user
+                                return res.status(400).json({ message: `Invalid Code, you have ${data.remainingPoints} attempts left, Please Login Again` });
+                            })
+                            .catch((rejRes) => {
+                                // Blocked
+                                const secBeforeNext = Math.ceil(rejRes.msBeforeNext / 60000) || 1;
+                                logger.error(`LoggingIn alert: Contact: ${req.body.contact} on IP: ${req.socket.remoteAddress} is Chocking Me !!`)
+                                return res.status(429).send(`Too Many Requests, Try to Login After ${String(secBeforeNext)} Minutes`);
+                            });
+
+
+                    } else {
+
+                        //create and assign a token once code is verified
+
+                        const token = jwt.sign({ _id: user._id, role: user.role }, process.env.TOKEN_SECRET, { expiresIn: 120 })
+
+
+                        const refreshToken = jwt.sign({ _id: user._id, role: user.role }, process.env.REFRESH_TOKEN_SECRET,
+                            { expiresIn: '1d' });
+
+                        redisClient.set(user._id.toString(), JSON.stringify({ refreshToken: refreshToken }));
+
+                        const userInfo = {
+                            _id: user._id,
+                            first_name: user.first_name,
+                            last_name: user.last_name,
+                            contact: user.contact,
+                            avatar: user.avatar
+                        }
+                        res.header('token', token).json({ 'token': token, 'refreshToken': refreshToken, 'user': userInfo });
                     }
-                    res.header('token', token).json({ 'token': token, 'refreshToken': refreshToken, 'user': userInfo });
-             }
-            })
-                
-        }}else{
+                })
+
+            }
+        } else {
             return res.status(400).json({ message: "Invalid request, Login again to get another code" })
         }
     } catch (error) {
-       return res.status(400).json({ message: error.message})
+        return res.status(400).json({ message: error.message })
     }
-    
-        
-   
+
+
+
 })
 
 //Deleting a Cleint
@@ -267,6 +279,8 @@ function createUsers(users) {
 router.get('/users/all', generalrateLimiterMiddleware, ensureAuth, async (req, res, next) => {
 
     try {
+        //verify user using OTP
+
         //check data in redisStore
         await redisClient.get('users', (err, result) => {
             if (err) {
@@ -319,15 +333,15 @@ router.get('/user/:id', ensureAuth, async (req, res) => {
 router.post('/users/logout', async (req, res) => {
     try {
         const user_id = req.body.user_id
-        
+
         //remove refresh token
-        await redisClient.del(user_id.toString(),(err,reply)=>{
-            if(err){
-               return res.status(400).json({message:err})
+        await redisClient.del(user_id.toString(), (err, reply) => {
+            if (err) {
+                return res.status(400).json({ message: err })
             }
-            return res.status(200).json({message:"Logged out successfull"})
+            return res.status(200).json({ message: "Logged out successfull" })
         });
-        
+
         //blacklist the access token
         await redisClient.set("BL_" + user_id.toString(), 'token')
 
